f78c663f07c5c02ee4dfa970d1a982bbbd7bcde7
mathough <mathough@gmail.com>
2019-08-06 13:44:54 -0400

    added instance for the admin vm

--- main.tf
+++ main.tf
@@ -1,5 +1,23 @@
 provider "google" {
   project = "var.project_id"
-  region = "us-east1"
-  zone = "us-east1-b"
+  region  = "us-east1"
+  zone    = "us-east1-b"
+}
+
+
+
+resource "google_compute_instance" "admin_vm" {
+  name         = "gcp-admin"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    netowrk = "google_compute_network.gcp_demo_vpc.self_link"
+    access_config {
+    }
+  }
 }
