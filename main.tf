be6488fb3aac66d3718bea3903a377fd2d8fa62c
mathough <mathough@gmail.com>
2019-08-07 05:04:11 -0400

    add a vpc network block

--- main.tf
+++ main.tf
@@ -27,3 +27,8 @@ resource "google_compute_network" "vpc_network" {
   name                    = "vpc-network"
   auto_create_subnetworks = "true"
 }
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "vpc-network"
+  auto_create_subnetworks = "true"
+}
