7acadaaacd9f3ef0dbc6687617b01551d0b3a796
mathough <mathough@gmail.com>
2019-08-27 10:15:28 -0400

    add VPC and subnetwork. remove kubernetes cluster for now

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ provider "google" {
   project = var.project_id
   region  = "us-east1"
   zone    = "us-east1-b"
+  version = "~> 2.12"
 }
 
-resource "google_compute_instance" "admin_vm" {
-  name         = "admin-vm"
+resource "google_compute_instance" "gcpte_vm" {
+  name         = "gcpte-vm"
   machine_type = "f1-micro"
 
   boot_disk {
@@ -14,18 +15,19 @@ resource "google_compute_instance" "admin_vm" {
     }
   }
   network_interface {
-    network = google_compute_network.vpc_network.self_link
+    network = google_compute_network.gcpte_vpc.self_link
     access_config {
     }
   }
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "vpc-network"
+resource "google_compute_network" "gcpte_vpc" {
+  name                    = "gcpte-vpc"
   auto_create_subnetworks = "true"
 }
-resource "google_compute_subnetwork" "vpc_subnet" {
-  name          = "vpc-subnet"
+
+resource "google_compute_subnetwork" "gcpte_subnet" {
+  name          = "gcpte-subnet"
   ip_cidr_range = "10.96.0.0/14"
-  network       = google_compute_network.vpc_network.self_link
+  network       = google_compute_network.gcpte_vpc.self_link
 }
